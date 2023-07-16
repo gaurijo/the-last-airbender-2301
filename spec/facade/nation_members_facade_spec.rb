5d7b0ac9441072edf1e3c13fdfa14bcbb8411da5
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-16 14:21:18 -0700

    Test: add more assertions to make sure nation members facade is returning expected data

--- spec/facade/nation_members_facade_spec.rb
+++ spec/facade/nation_members_facade_spec.rb
@@ -5,5 +5,7 @@ RSpec.describe NationMembersFacade, :vcr do
     response = NationMembersFacade.create_members("Air Nomads")
 
     expect(response).to be_a(Array)
+    expect(response.first).to be_a(NationMembers)
+    expect(response.first.allies).to eq(["Kya"])
   end
 end
