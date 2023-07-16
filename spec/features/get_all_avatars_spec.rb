381a2a0949aac567bbe91f11d8c7387f75b6b71f
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-15 17:37:12 -0700

    Test: avatars should have their image displayed on index page

--- spec/features/get_all_avatars_spec.rb
+++ spec/features/get_all_avatars_spec.rb
@@ -1,10 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe "as a visitor", :vcr do 
-  it 'has a list of avatars' do
+  it 'has a list of avatars, an image' do
 
     visit "/avatars"
 
     expect(page).to have_content("Aang")
+    expect(page).to have_css("img")
   end
 end
