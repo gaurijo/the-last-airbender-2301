5fd70d19322bc557891973a210be5620c28fbc88
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-16 13:52:37 -0700

    Test: Welcome Page should have a button for user to view Avatars page

---
+++ spec/features/welcome_spec.rb
@@ -0,0 +1,21 @@
+require 'rails_helper'
+
+RSpec.describe 'Welcome Page' do 
+  it 'has a button to see all the Avatars' do 
+
+    visit '/'
+
+    expect(page).to have_button('View All Avatars')
+  end
+
+  it 'takes the user to the /avatars path when clicking on the button', :vcr do 
+
+    visit '/'
+    
+    expect(current_path).to eq('/')
+
+    click_button('View All Avatars')
+
+    expect(current_path).to eq(avatars_path)
+  end
+end
