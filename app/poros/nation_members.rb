8a074fe33c991c5bc7c74077895dc6a1807fda36
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-17 12:52:42 -0700

    initialize nation members with attributes for allies/enemies/name/image

--- app/poros/nation_members.rb
+++ app/poros/nation_members.rb
@@ -1,7 +1,9 @@
 class NationMembers
-  attr_reader :enemies, :allies
+  attr_reader :enemies, :allies, :name, :photoUrl
   def initialize(data)
     @enemies = data[:enemies]
     @allies = data[:allies]
+    @name = data[:name]
+    @photoUrl = data[:photoUrl]
   end
 end
