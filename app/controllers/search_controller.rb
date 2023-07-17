65036f4de4f3aff7df02b6be6ee02052d7ffa917
Gauri Joshi <gauri.joshi@planetargon.com>
2023-07-17 12:52:12 -0700

    Feat: search controller takes in nation as param to return members

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,5 +1,8 @@
 class SearchController < ApplicationController
   def index
-    
+    @nation = params[:nation]
+    @members = NationMembersFacade.create_members(@nation)
+    # find param by nation id 
+    # based on nation id returns a list of members
   end
 end
